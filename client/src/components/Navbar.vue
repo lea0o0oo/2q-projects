31143e7e7c5420cb94f791dc72ac2812ef073285
Leo <le4ndro.fioretti@gmail.com>
2024-02-08 20:29:14 +0100

    🧌🧌🧌 :trollface: :trollface: :trollface: :rage1: :rage2: :rage3: :rage4:

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -81,6 +81,7 @@ function gotoURL(url) {
 
 <template>
   <div
+    id="navContainer"
     class="sticky flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-gray-800 dark:border-gray-700"
   >
     <nav
